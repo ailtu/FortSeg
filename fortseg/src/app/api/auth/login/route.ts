6804b9b6df49c19d09fd050606bda372d48ddb3e
Ailton <ailtonc.n64@gmail.com>
2026-05-21 14:17:27 -0300

    ajuste toda tela de login + req recaptcha

--- fortseg/src/app/api/auth/login/route.ts
+++ fortseg/src/app/api/auth/login/route.ts
@@ -1,65 +1,77 @@
 import { prisma } from "@/lib/prisma";
+
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
-
-    const verifyResponse = await fetch(
-        "https://www.google.com/recaptcha/api/siteverify",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type":
-                    "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams({
-                secret:
-                    process.env
-                        .RECAPTCHA_SECRET_KEY!,
-                response: body.captchaToken,
-            }),
-        }
-    );
-
-    const captchaData =
-        await verifyResponse.json();
-
-    if (!captchaData.success) {
-        return Response.json(
-            {
-                erro: "reCAPTCHA inválido",
-            },
-            {
-                status: 400,
-            }
-        );
-    }
-
     try {
+
         const body = await req.json();
 
-        const usuario = await prisma.usuario.findUnique({
-            where: {
-                email: body.email,
-            },
-        });
+        const verifyResponse = await fetch(
+            "https://www.google.com/recaptcha/api/siteverify",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type":
+                        "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams({
+                    secret:
+                        process.env
+                            .RECAPTCHA_SECRET_KEY!,
+                    response: body.captchaToken,
+                }),
+            }
+        );
 
-        if (!usuario) {
+        const captchaData =
+            await verifyResponse.json();
+
+        if (!captchaData.success) {
             return Response.json(
-                { erro: "Usuário não encontrado" },
-                { status: 404 }
+                {
+                    erro: "reCAPTCHA inválido",
+                },
+                {
+                    status: 400,
+                }
             );
         }
 
-        const senhaCorreta = await bcrypt.compare(
-            body.senha,
-            usuario.senha
-        );
+        const usuario =
+            await prisma.usuario.findUnique({
+                where: {
+                    email: body.email,
+                },
+            });
+
+        if (!usuario) {
+            return Response.json(
+                {
+                    erro:
+                        "Usuário não encontrado",
+                },
+                {
+                    status: 404,
+                }
+            );
+        }
+
+        const senhaCorreta =
+            await bcrypt.compare(
+                body.senha,
+                usuario.senha
+            );
 
         if (!senhaCorreta) {
             return Response.json(
-                { erro: "Senha inválida" },
-                { status: 401 }
+                {
+                    erro: "Senha inválida",
+                },
+                {
+                    status: 401,
+                }
             );
         }
 
@@ -67,7 +79,8 @@ export async function POST(req: Request) {
             {
                 id: usuario.id,
                 email: usuario.email,
-                permissao: usuario.permissao,
+                permissao:
+                    usuario.permissao,
             },
             process.env.JWT_SECRET!,
             {
@@ -77,19 +90,27 @@ export async function POST(req: Request) {
 
         return Response.json({
             token,
+
             usuario: {
                 id: usuario.id,
                 nome: usuario.nome,
                 email: usuario.email,
-                permissao: usuario.permissao,
+                cargo: usuario.cargo,
+                permissao:
+                    usuario.permissao,
             },
         });
     } catch (error) {
+        console.error(error);
+
         return Response.json(
-            { erro: "Erro interno do servidor" },
-            { status: 500 }
+            {
+                erro:
+                    "Erro interno do servidor",
+            },
+            {
+                status: 500,
+            }
         );
     }
-
-    const body = await req.json();
 }
